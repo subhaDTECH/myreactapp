aee39e5fb5148c98909142560af5057b447f87a8
subhaDTECH <suvaduleygba@gmail.com>
2021-01-14 10:35:25 +0530

    Updates

---
+++ precache-manifest.b9e8f6bd096ff280f8563f125cb9238e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7541b92db70a4963423dfd9fdc06794e",
+    "url": "/myreactapp/index.html"
+  },
+  {
+    "revision": "acc52d85121ca1b1e6cf",
+    "url": "/myreactapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8530b4ccb54adee63d47",
+    "url": "/myreactapp/static/css/main.e3289f8b.chunk.css"
+  },
+  {
+    "revision": "acc52d85121ca1b1e6cf",
+    "url": "/myreactapp/static/js/2.2811ce55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreactapp/static/js/2.2811ce55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8530b4ccb54adee63d47",
+    "url": "/myreactapp/static/js/main.775d1ecf.chunk.js"
+  },
+  {
+    "revision": "dc62612d9356efe2065c",
+    "url": "/myreactapp/static/js/runtime-main.24584c21.js"
+  }
+]);
